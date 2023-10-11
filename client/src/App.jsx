84ac339aeae1f0a7ccc075e95cab5790bd45d8b1
SHYAM T.R <t.r.shyam0007@gmail.com>
2023-10-11 19:57:38 +0530

    added headerPart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,17 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
+import Header from './components/Header';
 
 export default function App() {
   return( 
   <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/sign-in' element={ <Signin />} />
-      <Route path='/sign-up' element={ <Signup />} />
-    </Routes>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={ <Signin />} />
+        <Route path='/sign-up' element={ <Signup />} />
+      </Routes>
   </BrowserRouter>
   )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <nav>
+        <h1 className='font-black'>
+        <span className='text-amber-600	'>Name</span>
+        <span>Of</span>
+        <span>theShop</span>
+        </h1>
+        <rightnav>
+            <ul>
+                <li></li>
+            </ul>
+        </rightnav>
+    </nav>
+  )
+}
