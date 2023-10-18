462fefc2f9768685544341050591fc22d2b472ea
SHYAM T.R <t.r.shyam0007@gmail.com>
2023-10-18 18:34:58 +0530

    ConnectToMongoose

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+// mongoose.connect(process.env.MONGO)
+mongoose.connect("mongodb+srv://shyam0007:HQPXyDYP7Rfove76@react-e.hmomnpa.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+    console.log("connected");
+})
+.catch((err)=>{
+    console.log(err);
+})
 const app= express();
 
 app.listen(3000,()=>{
